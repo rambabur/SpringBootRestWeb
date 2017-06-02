2e72fc4b24507dc8c73045d152c144df8c76ffb6
rambabur <ramababur@gmail.com>
2017-06-01 21:54:18 -0400

    Added files
    initial commit

---
+++ src/main/java/com/etizen/boot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.etizen.boot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootRestWebApplication.class);
+	}
+
+}
---
+++ src/main/java/com/etizen/boot/SpringBootRestWebApplication.java
@@ -0,0 +1,12 @@
+package com.etizen.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestWebApplication.class, args);
+	}
+}
